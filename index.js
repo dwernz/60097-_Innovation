2ca450a5f98b5a8e19ec7415afe055cc8da4eea5
Daniel <62781672+dwernz@users.noreply.github.com>
2023-12-03 17:40:12 -0500

    update: pictures

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const nextBtn = document.getElementById("next-btn");
 const picture = document.getElementById("picture");
 
 //Change to have max number of photos.
-const pictureTotal = 5;
+const pictureTotal = 8;
 
 let pictureCount = 0;
 
 function checkCount() {
     if (pictureCount < 0) {
-        pictureCount = pitctureTotal - 1;
+        pictureCount = pictureTotal - 1;
     }
     else if (pictureCount > pictureTotal - 1) {
         pictureCount = 0;    
